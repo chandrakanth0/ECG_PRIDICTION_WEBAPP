445e725ad1ac9d392f97655cc6f2e97f2be0d14e
chandrakanth <chandrakanthchandru0799@gmail.com>
2025-05-24 21:44:45 +0530

    TEST2

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def index():
 #     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
