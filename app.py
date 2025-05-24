a14ce4751489776c0717ae0df8ca16683c7fa794
chandrakanth <chandrakanthchandru0799@gmail.com>
2025-05-24 23:03:53 +0530

    version 1.0

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-from flask import Flask, request, jsonify, render_template
-# import tensorflow as tf
+from flask import Flask, request, render_template
+import tensorflow as tf
 import numpy as np
-# from PIL import Image
+from PIL import Image
 import os
 
 app = Flask(__name__)
 
-# Load your model
-# model = tf.keras.models.load_model("Vgg16_ecg_model.keras")
+# Load model and define class labels manually (based on training data)
+model = tf.keras.models.load_model("Vgg16_ecg_model.keras")
+class_labels = ['class1', 'class2', 'class3', 'class4']  # 🔁 Replace with actual class folder names
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     file = request.files['file']
-#     image = Image.open(file.stream).resize((224, 224))
-#     image = np.array(image) / 255.0
-#     image = image.reshape(1, 224, 224, 3)
-#     prediction = model.predict(image)
-#     result = "Positive" if prediction[0][0] > 0.5 else "Negative"
-#     return jsonify({'result': result})
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    image = Image.open(file.stream).resize((224, 224)).convert('RGB')
+    image = np.array(image) / 255.0
+    image = image.reshape(1, 224, 224, 3)
+
+    prediction = model.predict(image)
+    predicted_class = np.argmax(prediction[0])
+    predicted_label = class_labels[predicted_class]
+    probabilities = dict(zip(class_labels, [round(float(p) * 100, 2) for p in prediction[0]]))  # in %
+
+    return render_template("result.html", predicted_label=predicted_label, probabilities=probabilities)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
